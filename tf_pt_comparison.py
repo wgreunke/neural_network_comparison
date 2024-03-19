04f6450df4b81115f6cf8b940a1983caac3099ed
wgreunke <wgreunke@gmail.com>
2024-03-19 11:53:33 -0700

    Create tf_pt_comparison.py

---
+++ tf_pt_comparison.py
@@ -0,0 +1,8 @@
+#This file compares the speed and accuracy of TensorFlow and PyTorch on the NIST handwriting dataset
+#Ward Greunke
+#Process
+#Import NIST Dataset
+#Run the analysis using Tensorflow, record time and accuracy
+#Run the analyis usin PyTorch record time and accuracy
+
+
